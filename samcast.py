fdb091872a9f779d48b3cd688bd75f6c874b3e66
Sam Howson <sam@samhowson.co.uk>
2016-10-24 16:45:37 +0100

    I finally got around to updating this, and correcting my horrible code from 4 years ago. Should be much more robust now.

--- samcast.py
+++ samcast.py
@@ -3,70 +3,88 @@
 
 """
 Created by Sam Howson on 2012-07-21.
-
-This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/"""
+"""
 
 import sys
 import os
 import libxml2, libxslt
-import re, ConfigParser
+import ConfigParser
 import urllib
+import argparse
+import requests
+import re
 
+def sampodder(podcast_dir, podcast, config, logstring):
+    rss_feed = config.get(podcast, 'rss-feed')
+    thispodcastdir = os.path.join(podcast_dir, podcast)
+    
+    if not os.path.exists(thispodcastdir):
+        os.makedirs(thispodcastdir)
+    
+    podnumber = config.get(podcast, 'podnumber')
+    if not podnumber:
+        podnumber = -1
+    else:
+        int(podnumber)
 
-def sampodder(podcast):
-	title = podcast
-	rss_feed = config.get(podcast, 'rss-feed')
-	thispodcastdir = os.path.join(podcast_dir + podcast)
-	
-	if not os.path.exists(thispodcastdir):
-	        os.makedirs(thispodcastdir)
-	
-	try:
-		podnumber = int(config.get(podcast, 'podnumber'))	
-	except:
-		podnumber = -1
-	
-	try:
-		styledoc = libxml2.parseFile(samcast_dir + "parse_enclosure.xsl")
-		style = libxslt.parseStylesheetDoc(styledoc)
-		doc = libxml2.parseFile(rss_feed)
-		result = style.applyStylesheet(doc, None)
-		podcast_episodes = style.saveResultToString(result)
-		podcast_episodes = podcast_episodes.split('\n')[:-1][:podnumber]
-		style.freeStylesheet()
-		doc.freeDoc()
-		result.freeDoc()
-		
-	except: print '''Couldn't get the podcasts using libxslt :('''
-		#Perhaps could try using wget here instead, as with the bashpodder script.
+    styledoc = libxml2.parseFile(os.path.join(os.getcwd(), "parse_enclosure.xsl"))
+    style = libxslt.parseStylesheetDoc(styledoc)
+    doc = libxml2.parseFile(rss_feed)
+    result = style.applyStylesheet(doc, None)
+    podcast_results = style.saveResultToString(result)
+    podcast_episodes = podcast_results.split('\n')[:-1][:podnumber]
+    
+    style.freeStylesheet()
+    doc.freeDoc()
+    result.freeDoc()
+    
 
-	for episode in podcast_episodes:
-		if re.search(episode, logstring):
-			pass
-		else:
-			episode_title=re.split('\/|=|\?', episode)[-1]
-			#If you want to do a dry-run, just comment out the line below.
-			urllib.urlretrieve(episode, thispodcastdir + '/'+ episode_title)
-			download_log.write(episode + '\n')
+    for episode in podcast_episodes:
+        if episode in logstring:
+            return None
+        else:
+            episode_title = re.split('\/|=|\?', episode)[-1]
+            #If you want to do a dry-run, just comment out the line below.
+            print(episode, thispodcastdir + '/'+ episode_title)
+            episode_path = os.path.join(thispodcastdir, episode_title)
+            response = requests.get(episode, stream=True)
+            with open(episode_path, 'wb') as ep:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        ep.write(chunk)
+            return episode_path
+
 
 
 if __name__ == "__main__":
-	
-	home=os.path.expanduser('~')
-	podcast_dir = '/storage/greyarea/podcasts'
-	samcast_dir = os.path.join(home + '/Samcast/')
-	config_file = os.path.join(samcast_dir + 'sp.conf')
-	config = ConfigParser.ConfigParser()
-	config.read(config_file)
-	podcasts = config.sections()
-	
-	if not os.path.exists(podcast_dir + '/downloads.log'):
-		open(podcast_dir + '/downloads.log', 'w+').close()
+   
+    parser = argparse.ArgumentParser(description='A podcast downloader script.')
+    parser.add_argument('config', help='The location of the configuration file.')
+    parser.add_argument('-p', '--podcast-dir', default=os.getcwd(), dest='podcast_dir', help='The location of the directory to save podcasts')
+    parser.add_argument('-d', '--download-log', dest='download_log', help='The location of the directory to save podcasts')
+    args = parser.parse_args()
 
-	with  open(podcast_dir + '/downloads.log', 'r+') as download_log:
-		logstring = download_log.read()
-		for p in podcasts:
-			sampodder(p)
-		
-		
+
+
+    if not args.download_log:
+        download_log = os.path.join(args.podcast_dir, 'downloads.log')
+
+    config = ConfigParser.ConfigParser()
+    config.read(args.config)
+    podcasts = config.sections()
+  
+    if not os.path.exists(download_log):
+        open(download_log, 'w+').close()
+        
+
+    with open(download_log, 'rw+') as dl:
+        logstring = dl.read()
+        for podcast in podcasts:
+            try:
+                podcast_file = sampodder(args.podcast_dir, podcast, config, logstring)
+                print(podcast_file)
+                dl.write(podcast_file + '\n')
+            except libxml2.parserError:
+                pass
+        
+        
